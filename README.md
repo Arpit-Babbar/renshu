341cc839526f8a5dcc21a471c8e8e830bee230db
Arpit Babbar <arpitbabbar@gmail.com>
2020-10-24 20:29:54 +0530

    Made code readable

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # arpit_practise
  
-I would write the things that I feel would improve the code. 
+I would write the things that I feel would improve the code.
+
+1) Use reference variables for initial data?
+
+Is there a shortcut to changing all occurences of a function? I realized that I wish to change the order of inputs of the constructor. Can the editor do it?
--- linear_convection_1d.cc
+++ linear_convection_1d.cc
@@ -8,34 +8,38 @@ using namespace std;
 class Linear_Convection
 {
     public:
-      Linear_Convection(double x_min0, double x_max0, int n_points0, double cfl0, double coefficient0,vector<double> initial_data);
+      Linear_Convection(double x_min, double x_max, double coefficient   //PDE Parameters
+                        ,vector<double> initial_data                     //PDE Data
+                        ,int n_points, double cfl);                      //Algorithm parameters
       void run();
     
     private:
-      void make_grid();
-      //void setup_system(); Not needed
-      void solve_system(); //This should give the solution at time t = n_iterations * time_step
+      void make_grid();    //Grid is needed for writing output
+      void solve_system(); //Gives solution at next time step
       
       double coefficient;double x_min;double x_max; //PDE parameters
 
-      double n_points; double cfl; double time_step; int n_iterations; //Numerical algorithm parameters
+      double n_points; double cfl; double time_step; int n_iterations; //Numerical algorithm 
+      //parameters
       vector<double> grid;
 
-      vector<double> solution_old; //solution at time t-1
-      vector<double> solution_t; //solution at time t
+      vector<double> solution_old;   //solution at previous step
+      vector<double> solution_new;   //solution at present step
 };
-Linear_Convection::Linear_Convection(double x_min0, double x_max0, int n_points0, double cfl0,double coefficient0, vector<double> initial_data)
-  : x_min(x_min0), x_max(x_max0), n_points(n_points0) , cfl(cfl0)
-   , coefficient(coefficient0)
-   , time_step(n_points/(coefficient * n_points))
-   , solution_t(n_points) //specifying size
-   , solution_old(initial_data) //specifying the full vector
+Linear_Convection::Linear_Convection(double x_min, double x_max, double coefficient     
+                    ,vector<double> initial_data
+                    ,int n_points, double cfl)
+  : x_min(x_min), x_max(x_max), n_points(n_points) , cfl(cfl)
+   , coefficient(coefficient)                           //Assigning the values
+   , time_step(n_points/(abs(coefficient) * n_points))  //time_step = (cfl * h)/ |a|
+   , solution_new(n_points) //specifying size of solution_new
+   , solution_old(initial_data) //specifying the full vector solution_old
 {}
 void Linear_Convection::make_grid()
 {
     for (int j = 0; j < n_points ; j++)
     {
-        double h = (x_max - x_min)/n_points;
+        double h = (x_max - x_min)/n_points;        //Distance between grid points = 1/N
         grid.push_back(x_min + j*h);
     }   
     for (int j = 0; j < grid.size() ; j++)
@@ -51,10 +55,12 @@ void Linear_Convection::solve_system()
 {
     for (int j = 0; j < n_points; ++j) //Loop over grid points
     {
-        solution_t[j] = solution_old[j] - 0.5 * cfl * (solution_old[j+1] - solution_old[j-1]) + 0.5 * cfl * cfl * (solution_old[j-1] - 2 * solution_old[j] + solution_old[j+1]);
+        solution_new[j] = solution_old[j]
+                        - 0.5 * cfl * (solution_old[j+1] - solution_old[j-1])
+                        + 0.5 * cfl * cfl * (solution_old[j-1] - 2 * solution_old[j] + solution_old[j+1]);
         
     }
-    solution_old = solution_t;
+    solution_old = solution_new;
 }
 
 void Linear_Convection::run()
@@ -98,15 +104,18 @@ void Linear_Convection::run()
 int main()
 {
     //set parameters
-    double x_min = 0; double x_max = 1.0; double coefficient = 1.0;
-    double n_points = 30; double cfl = 0.5;
-    vector<double> initial_data(n_points);
+    double x_min = 0; double x_max = 1.0; double coefficient = 1.0; //PDE Parameters
+    double n_points = 30; double cfl = 0.5;                         //Num. Alg. Parameters
+    vector<double> initial_data(n_points);                          //Initial Data
     for (auto value_i = initial_data.begin(); value_i != initial_data.end(); ++value_i) 
     {
         *value_i = 1.0;     
-    }
+    }                                                               
+    
+    
+    
     //make_grid
-    Linear_Convection solver(x_min,x_max,n_points,cfl, coefficient, initial_data);
+    Linear_Convection solver(x_min,x_max,coefficient,initial_data, n_points,cfl);
     solver.run();
     return 0;
 }
