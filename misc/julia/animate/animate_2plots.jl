2ee98bb03dcf83a46f9c07b3ad043a20d7de9e28
Arpit-Babbar <arpitbabbar@gmail.com>
2021-06-24 08:24:01 +0530

    Fix plot improvement

--- misc/julia/animate/animate_2plots.jl
+++ misc/julia/animate/animate_2plots.jl
@@ -7,12 +7,16 @@ function main()
    Tf = 10.0
    t  = 0.0
    u  = sin.(2.0*π*x.-t)
+   v  = cos.(2.0*π*x.-t)
    p = plot(x,u)
+   plot!(p,x,v)
    anim=Animation()
    while t<Tf
       t += dt
       u  = sin.(2.0*π*(x.-t))
+      v  = cos.(2.0*π*(x.-t))
       p[1][1][:y] = u
+      p[1][2][:y] = v
       frame(anim)
    end
    plot(p) # final plot
